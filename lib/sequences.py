d9f6b4953393e1b71eba27cf1f11974ced98faf0
Luke <luke.wangai@student.moringaschool.com>
2025-05-09 10:12:57 +0300

    sequences complete

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
 
-def print_fibonacci(length):
-    pass
+def print_fibonacci(n):
+    if n <= 0:
+        print([])
+        return
+    elif n == 1:
+        print([0])
+        return
+    
+    fib = [0,1]
+
+    while len(fib) < n:
+        fib.append(fib[-2] + fib[-1])
+    print(fib)
+
+
+print_fibonacci(9)
